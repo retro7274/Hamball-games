e6d289ed3f2c7a6d032dfba30573f9a42d69989f
retro7274 <wintertech213@gmail.com>
2024-01-10 13:37:17 -0800

    the game fr
    help please

---
+++ Hamball/Assets/Scripts/Playermovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float MoveSmoothTime;
+    public float GravityStrength;
+    public float JumpStrength;
+    public float WalkSpeed;
+    public float RunSpeed;
+
+    private CharacterController Controller;
+    private Vector3 CurrentMoveVelocity;
+    private Vector3 MoveDampVelocity;
+
+    private Vector3 CurrentlyForceVelocity;
+
+
+    void Start()
+    {
+       Controller = GetComponent<CharacterController>(); 
+    }
+
+    void Update()
+    {
+        Vector3 PlayerInput = new Vector3
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical")
+        };
+
+        if (PlayerInput.magnitude > 1f)
+        {
+            PlayerInput.Normalize();
+        }
+
+        Vector3 MoveVector = transform.TransformDirection(PlayerInput);
+        float CurrentSpeed = Input.GetKey(KeyCode.LeftShift) ? RunSpeed : WalkSpeed;
+
+        CurrentMoveVelocity = Vector3.SmoothDamp(
+            CurrentMoveVelocity,
+            MoveVector * CurrentSpeed,
+            ref MoveDampVelocity,
+            MoveSmoothTime
+        );
+
+        Controller.Move(CurrentMoveVelocity * Time.deltaTime);
+
+        Ray groundCheckRay = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(groundCheckRay, 1.1f))
+        {
+            CurrentlyForceVelocity.y = -2f;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                CurrentlyForceVelocity.y = JumpStrength;
+            }
+        }
+        else
+        {
+            CurrentlyForceVelocity.y -= GravityStrength * Time.deltaTime;
+        }
+    }
+}
